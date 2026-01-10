d977740c1c30401e74fa6b17ed5377cd6518b0ca
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-10 14:33:59 +0100

    Variables

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 # Getting Started
-
+"""
 print("Hello World!")
 print("Hi World!")
+"""
+
+# Variables
+salary = 100000 # in RSD
+bonus_rate = .10 # 10%
+employee_name = "Dean"
+company_name = "CompTech"
+
+print(f"Employee Name: {employee_name}\nWorks For: {company_name}\nSalary: {salary}\n"
+      f"Salary with bonus rate: {(salary * (1 + bonus_rate)):.0f}")
+
+input()
+
+job_id = 101
+job_title = "Data Analyst"
+job_salary = 125000
+job_wfh = True # wfh = work from home
+
+print("Job Description")
+print("+++++++++++++++")
+print("Job ID:        ", job_id)
+print("Job Title:     ", job_title)
+print("Job Salary:    ", job_salary)
+print("Job WFH:       ", job_wfh)
