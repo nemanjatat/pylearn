1680e0e659c031b17f8d4fbd437c63b17a0e26a4
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-11 16:16:30 +0100

    Python Terms

--- main.py
+++ main.py
@@ -26,3 +26,102 @@ print("Job ID:        ", job_id)
 print("Job Title:     ", job_title)
 print("Job Salary:    ", job_salary)
 print("Job WFH:       ", job_wfh)
+
+# Python Terms
+
+# Object: a data record with fields; an instance of a class
+"""
+salary = 100000
+print(salary)
+input()
+print(type(salary))
+input()
+print(type("Data Analyst"))
+input()
+help(str)
+"""
+# Variables: The Reference to Objects
+# Variables names themselves point to an object
+# Variable: a defined name that references an object
+"""
+job_title = "Data Analyst"
+job_location = "United States"
+job_salary = 90000
+
+print(id(job_title))
+print(id(job_location))
+
+# example of variables pointing (or identify) a specific object
+job_1 = "Data Analyst"
+job_2 = "Data Analyst"
+
+print(id(job_1))
+print(id(job_2))
+input()
+
+job_3 = job_1
+
+print(id(job_1))
+print(id(job_2))
+"""
+# you can assign anything to variables
+my_print_func = print
+
+my_print_func("Hi hello!")
+
+# Function: A reusable piece of code that performs a special task.
+
+# built-in function
+print("Hi hello!")
+
+# user defined function
+def release_date():
+      print("January 22nd")
+
+release_date()
+
+def movie_info(name, year, rating):
+      print(f"Name:     {name}\nYear:     {year}\nRating:   {rating}")
+
+movie_info("Seven Samurai", 1954, 8.6)
+
+# Classes: The Template of Objects
+# Class: A template for creating objects (records).
+
+class JobPost:
+      def __init__(self, title, location, salary):
+            self.title = title
+            self.location = location
+            self.salary = salary
+
+print(JobPost) # instance of a class
+
+# Attributes: The Variables of an Object
+# Attribute: A field in a record, defined by its class
+
+class Animal:
+      def __init__(self, species, diet, lifespan):
+            self.species = species
+            self.diet = diet
+            self.lifespan = lifespan
+
+animal_1 = Animal("Bald Eagle", "Carnivore", "20-30")
+print(animal_1.species)
+
+# The Functions of an Object
+# Method: A function defined inside a class that operates on its objects.
+
+class Device:
+      def __init__(self, type, name, price):
+            self.type = type
+            self.name = name
+            self.price = price
+      def display_info(self): # method (function of a class)
+            print(f"Type:      {self.type}\nName:      {self.name}\nPrice:     {self.price}")
+
+laptop = Device("Laptop", "Asus TUF", 100000)
+laptop.display_info()
+
+print(help(int)) # we can see __add__ defined method in the int class
+
+print(laptop.price.__add__(10000))
