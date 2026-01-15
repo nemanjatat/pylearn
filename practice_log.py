6f224402f07c8a1fb6e4faa3b990b93d6fde591f
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-15 13:57:49 +0100

    Practice III

--- practice_log.py
+++ practice_log.py
@@ -1,18 +1,4 @@
-available_movies = ["THE LAST SAMURAI", "300", "KNIVES OUT"]
+available_movies = ["THE LAST SAMURAI", "300", "KNIVES OUT", "Mission Impossible"]
 
-while True:
-    print("Enter movie you want to watch:", end=" ")
-    user_movie_input = input()
-
-    if available_movies.__contains__(user_movie_input.upper()):
-        print("That movie is available!")
-    else:
-        print("We don't have that movie!")
-
-    print("Would you like to check for another movie? (Y/N)")
-    user_another_movie_answer = input()
-
-    if user_another_movie_answer.upper() == "Y":
-        pass
-    else:
-        break
+for movie in available_movies:
+    print(movie)
