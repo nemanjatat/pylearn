24427ad4c1cf152859972bc9e6066eff3b223c69
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-01-18 23:28:40 +0100

    PRACTICE IV

--- practice_log.py
+++ practice_log.py
@@ -1,4 +1,8 @@
 available_movies = ["THE LAST SAMURAI", "300", "KNIVES OUT", "Mission Impossible"]
+supported_devices = ["SMART TV", "PC", "ANDROID", "IOS"]
 
 for movie in available_movies:
     print(movie)
+
+for supported_device in supported_devices:
+    print(supported_device)
