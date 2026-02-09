d663d4fbcaf4d5f2b2866a0eb4ecb67474cd330f
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-09 12:42:31 +0100

    movies added and displayed

--- movies.py
+++ movies.py
@@ -1,4 +1,17 @@
-movieList = {
-    'id': 1,
-    'name': "Django Unchained"
-}
+movieList = [
+    {
+        'id': 1,
+        'name': "Django Unchained"
+    },
+    {
+        'id': 2,
+        'name': "Good Will Hunting"
+    },
+    {
+        'id': 3,
+        'name': "Gladiator"
+    }
+]
+
+for movie in movieList:
+    print(f"ID: {movie['id']}; Name: {movie['name']}")
