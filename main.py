594a0ca793c5c08980e20eb4e0112cdc399e67b6
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-12 17:58:53 +0100

    4. user input

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-# typecasting = the process of converting a variable from one data type to another
-#               str(), int(), float(), bool()
+# input() = a function that prompts the user to enter data
+#           returns the entered data as a string
 
-movie_name = "Crime 101"
-release_year = 2026
-score = 3.3
-in_cinemas = True
+new_item = input("What new item are you adding to the collection?: ")
+item_stock = int(input(f"How many {new_item}s are you adding to the collection?: "))
 
-print(f"Movie name's original type: {type(movie_name)}")
-movie_name = bool(movie_name)
-print(f"Movie name's altered type: {type(movie_name)}")
+print(f"You already have an extra {new_item}, +1")
+item_stock += 1
 
-print(f"Release year's original type: {type(release_year)}")
-release_year = float(release_year)
-print(f"Release year's altered type: {type(release_year)}")
-
-print(f"Score's original type: {type(score)}")
-score = int(score)
-print(f"Score's altered type: {type(score)}")
-
-print(f"in_cinema's original type: {type(in_cinemas)}")
-in_cinemas = str(in_cinemas)
-print(f"in_cinema's altered type: {type(in_cinemas)}")
-
-print("Conclusion\n============")
-print(f"Movie Name: {movie_name}")
-print(f"Release Year: {release_year}")
-print(f"Score: {score}")
-print(f"In Cinemas: {in_cinemas}")
+print("============\nITEM INFO\n============")
+print(f"New item added: {new_item}\nCurrent stock: {item_stock}")
