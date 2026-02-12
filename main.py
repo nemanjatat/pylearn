346a4ab939eb92cca7779ba56f788e7546ce514c
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-12 17:30:40 +0100

    3. type casting

--- main.py
+++ main.py
@@ -1,65 +1,29 @@
-# variable = a container for a value (string, integer, float, boolean)
-#            a variable behaves as if it was the value it containts
+# typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
 
-# string
-name = "nem"
-item = "pen"
-email = "nem@notrealemail.com"
+movie_name = "Crime 101"
+release_year = 2026
+score = 3.3
+in_cinemas = True
 
-print(f"Your name is: {name}")
-print(f"You've picked up a new item!\nItem: {item}")
-print(f"That guy's email is: {email}")
+print(f"Movie name's original type: {type(movie_name)}")
+movie_name = bool(movie_name)
+print(f"Movie name's altered type: {type(movie_name)}")
 
-# integers
-age = 29
-stock = 5
-num_of_people_at_event = 1549
+print(f"Release year's original type: {type(release_year)}")
+release_year = float(release_year)
+print(f"Release year's altered type: {type(release_year)}")
 
-print(f"Your age is: {age}")
-print(f"Current stock: {stock}")
-print(f"The number of people at the event is: {num_of_people_at_event}")
+print(f"Score's original type: {type(score)}")
+score = int(score)
+print(f"Score's altered type: {type(score)}")
 
-# floats
-price = 10.99
-gpa = 4.4
-distance = 10.5
+print(f"in_cinema's origianl type: {type(in_cinemas)}")
+in_cinemas = str(in_cinemas)
+print(f"in_cinema's altered type: {type(in_cinemas)}")
 
-print(f"The price for that is {price}$")
-print(f"Their gpa is: {gpa}")
-print(f"That location is {distance}km away")
-
-# booleans
-is_student = False
-for_sale = True
-is_online = True
-
-if is_student:
-    print("You are a student")
-else:
-    print("You are NOT a student")
-
-if for_sale:
-    print("That item is for sale")
-else:
-    print("That item is NOT for sale")
-
-if is_online:
-    print("Friend is currently online")
-else:
-    print("Friend is currently offline")
-
-# assignment
-
-username = "@nem"
-year = 2026
-pi = 3.14
-is_admin = True
-
-print("=======================")
-print("loading user info")
-print("=======================")
-
-print(f"Username: {username}")
-print(f"Account created in year: {year}")
-print(f"Pi value: {pi}")
-print(f"User is admin: {is_admin}")
+print("Conclusion\n============")
+print(f"Movie Name: {movie_name}")
+print(f"Release Year: {release_year}")
+print(f"Score: {score}")
+print(f"In Cinemas: {in_cinemas}")
