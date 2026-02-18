0b224a459b65db05913fe23b1373240919dd77cf
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-18 16:01:20 +0100

    6. arithmetic & math pt3 - exercise 3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import math
 
-radius = float(input(f"Enter the radius of a circle: "))
+a = float(input("Enter side A: "))
+b = float(input("Enter side B: "))
 
-area = 2 * math.pi * pow(radius, 2)
+c = math.sqrt(pow(a, 2) + pow(b, 2))
 
-print(f"The area of the circle is: {round(area, 2)}cm")
+print(f"Side C = {c}")
