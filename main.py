980e33cd9f0244fe21dc940cfe12d1740223322d
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-18 17:47:06 +0100

    7. if statements pt2

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-# if    = do some code only IF some condition is true
-# else  = do something else
+response = input("Would you like some food? (Y/N): ")
 
-age = int(input("Enter your age to finishing signing up: "))
-
-if age >= 100:
-    print("You are too old to sign up!")
-elif age >= 18:
-    print("You are now signed up!")
-elif age < 0:
-    print("You haven't been born yet!")
+if response == "Y":
+    print("Have some food!")
 else:
-    print("You must be over 18 or older to sign up!")
+    print("No food for you!")
