278ce8ca157ee0264ac7d0410cd427e4b5bba03b
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-20 15:19:57 +0100

    8. calculator program v1

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 if operator == "+":
-    pass
+    print(f"{num1} + {num2} = {num1 + num2}")
 elif operator == "-":
-    pass
+    print(f"{num1} - {num2} = {num1 - num2}")
 elif operator == "*":
-    pass
+    print(f"{num1} * {num2} = {num1 * num2}")
 elif operator == "/":
-    pass
+    print(f"{num1} / {num2} = {num1 / num2}")
 else:
-    pass
+    print("Something went wrong!")
