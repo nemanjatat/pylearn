f8814eead8f102c7cc2ac140150d6bdfefee08fc
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-22 17:04:33 +0100

    13. string methods pt2 - comprehensive list of string methods

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-name = input("Enter your full name: ")
-
-print(len(name)) # is actually a function, not a method
-print(name.find("a"))
-print(name.rfind("a"))
-print(name.capitalize())
-print(name.upper())
-print(name.lower())
-print(name.isdigit())
-print(name.isalpha())
-
-phone_number = input("Enter your phone number: ")
-
-print(phone_number.count("-"))
-print(phone_number.replace("-", ""))
+help(str)
