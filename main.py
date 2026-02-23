5df07dd384c6f95b2d511737687229d2021cd636
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-23 17:30:29 +0100

    14. string indexing pt3

--- main.py
+++ main.py
@@ -3,27 +3,6 @@
 
 credit_number = "1234-5678-9012-3456"
 
-last_digits = credit_number[-4:]
-print(last_digits)
-
-# or
-last_digits = credit_number[15:]
-print(last_digits)
-
-print("============")
-print("============")
-print("============")
-
-print(f"XXXX-XXXX-XXXX-{last_digits}")
-user_input = input("fill in the missing numbers without (-):\n")
-
-digits_no_hyphen = credit_number[0:15].replace("-", "")
-
-if user_input == digits_no_hyphen:
-    print("Correct!")
-else:
-    while True:
-        user_input = input("Try again: ")
-        if user_input == digits_no_hyphen:
-            print("Correct!")
-            break
+# to reverse a string:
+credit_number_reversed = credit_number[::-1]
+print(credit_number_reversed)
