7c23fe4843418d789d05dcbe096549854a65aad5
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-23 18:12:55 +0100

    PRACTICE VI

--- practice_log.py
+++ practice_log.py
@@ -1,8 +1,22 @@
-cities = {
-    "Tokyo": 14,
-    "Madrid": 3,
-    "Berlin": 4
-}
+items = []
 
-cities.pop("Madrid")
-print(sum(cities.values()))
+while True:
+    print("What would you like to do?")
+    user_input = input("1. show list of items\n2. add an item\n3. quit\nChoose: ")
+
+    if user_input == "1":
+        if not items:
+            print("The list is currently empty")
+        else:
+            print("Here is a list of your items:\n")
+            for item in items:
+                print(item)
+    elif user_input == "2":
+        new_item = input("What are you adding?: ")
+        items.append(new_item)
+    elif user_input == "3":
+        break
+    else:
+        print("Invalid command")
+
+print("Program exited")
