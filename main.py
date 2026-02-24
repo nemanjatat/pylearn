9d7e4f1c9abe8f965023b7b978f826ce787729dd
Nemanja Tatalovic <nemanja.tatalovic.it@gmail.com>
2026-02-24 16:28:36 +0100

    15. format specifiers

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-# indexing = accessing elements of a sequence using [] (indexing operator)
-#            [start : end : step]
+def print_lines():
+    print("===============")
+    print("===============")
+    print("===============")
 
-credit_number = "1234-5678-9012-3456"
+# format specifiers = {:flags} format a value based on what
+#                              flags are inserted
 
-# to reverse a string:
-credit_number_reversed = credit_number[::-1]
-print(credit_number_reversed)
+# .(number)f = round to that many decimal places (fixed point)
+# :(number) = allocate that many spaces
+# :03 = alocate and zero pad that many spaces
+# :< = left justify
+# :> = right justify
+# :^ = center align
+# :+ = use a plus sign to indicate positive value
+# := = place sign to leftmost position
+# :  = insert a space before positive numbers
+# :, = comma separator
+
+price1 = 3.45879
+price2 = -42.689
+price3 = 12.45
+
+# round to n many decimal places
+print(f"Price 1 is ${price1:.2f}")
+print(f"Price 2 is ${price2:.2f}")
+print(f"Price 3 is ${price3:.2f}")
+
+print_lines()
+# allocate n spaces
+print(f"Price 1 is ${price1:10}")
+print(f"Price 2 is ${price2:10}")
+print(f"Price 3 is ${price3:10}")
+
+print_lines()
+# zero padded
+print(f"Price 1 is ${price1:010}")
+print(f"Price 2 is ${price2:010}")
+print(f"Price 3 is ${price3:010}")
